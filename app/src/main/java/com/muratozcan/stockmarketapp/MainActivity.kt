099d2b133462fc8c580658299ca39c7a77f6da08
muratozcan8 <mrtzcn08@gmail.com>
2024-02-28 22:50:28 +0300

    Initial commit

---
+++ app/src/main/java/com/muratozcan/stockmarketapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.muratozcan.stockmarketapp
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.muratozcan.stockmarketapp.screens.LoginPage
+import com.muratozcan.stockmarketapp.screens.RegisterPage
+import com.muratozcan.stockmarketapp.ui.theme.StockMarketAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        setContent {
+            StockMarketAppTheme {
+                LoginApplication()
+            }
+        }
+    }
+
+    @Composable
+    fun LoginApplication(){
+        val navController = rememberNavController()
+
+        NavHost(navController = navController, startDestination = "login_page", builder = {
+            composable("login_page", content = { LoginPage(navController = navController) })
+            composable("register_page", content = { RegisterPage(navController = navController) })
+        })
+    }
+}
+
